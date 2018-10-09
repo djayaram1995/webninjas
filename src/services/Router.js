4646053b11b30976af8bba305d87949deb2f6c89
Dakshina Murthy <577782@cognizant.com>
2018-10-09 20:07:37 +0530

    Router changes

--- src/services/Router.js
+++ src/services/Router.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import App from '../scenes/HomePage/App';
 import ContactUs from '../scenes/ContactUs';
 import Services from '../scenes/Services';
-import logo from '../assets/logo.svg';
 import './Router.css'
 
 const Router = () => {
     return (
         <div>
-            <div>
-                <div className="titleWebay">
-                    <div className="d-inline"><img src={logo} alt="logo" style={{height: '50px', width: '50px'}} /></div>
-                    <div className="d-inline spanWeb">Webay</div>
-                    
-                </div>
-            </div>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path='/' component={App} />
-                    <Route path='/services' component={Services} />
-                    <Route path='/contactus' component={ContactUs} />
-                </Switch>
-            </BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={App} />
+                <Route path='/services' component={Services} />
+                <Route path='/contactus' component={ContactUs} />
+            </Switch>
         </div>
     );
 }
