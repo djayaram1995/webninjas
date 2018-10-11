40a57691efe0a3d217b68f4dfa9a07f0dc7264ea
unknown <jayaramdeenu@gmail.com>
2018-10-11 09:36:40 +0530

    eslinting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RouterContainer from './services'
+import RouterContainer from './services';
 import * as serviceWorker from './serviceWorker';
 
 
