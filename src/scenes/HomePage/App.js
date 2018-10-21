a0fd9df903d94d213eefcb6578b0dc5447d783ff
unknown <jayaramdeenu@gmail.com>
2018-10-21 08:12:13 +0530

    Changing alphabet animation

--- src/scenes/HomePage/App.js
+++ src/scenes/HomePage/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mounted: true
+    }
+  }
+
+  componentDidMount() {
+    this.AnimatedText("code", ["understand", "design", "develop", "deploy"]);
+
+  }
+  AnimatedText(target, texts, changeInterval, updateInterval, onTextChanged) {
+    let currentText = parseInt(Math.random() * texts.length);
+    let areaText = texts[0];
+    this.t1 = setInterval(() => {
+      let c = parseInt(Math.random() * Math.max(texts[currentText].length, areaText.length));
+      let s = texts[currentText][c];
+      if (typeof s == 'undefined') s = " ";
+      while (areaText.length < c) areaText += " ";
+      let newText = (areaText.slice(0, c) + s + areaText.slice(c + 1)).trim();
+      let diff = !(newText === areaText);
+      areaText = newText;
+      if (onTextChanged && diff) onTextChanged();
+      document.getElementById(target).innerHTML = areaText.length === 0 ? "&nbsp;" : areaText;
+    }, updateInterval ? updateInterval : 50);
+    this.t2 = setInterval(() => {
+      currentText = parseInt(Math.random() * texts.length);
+    }, changeInterval ? changeInterval : 2500);
+    this.stop = function () { clearInterval(this.t1); clearInterval(this.t2); }
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="changingFont">
+          We <span id="code"></span>
+        </div>
       </div>
     );
   }
