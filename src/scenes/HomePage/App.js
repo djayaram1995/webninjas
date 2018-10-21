659534350d7fdbebbcc12dc9069db7551add7053
unknown <jayaramdeenu@gmail.com>
2018-10-21 17:55:45 +0530

    Design changes

--- src/scenes/HomePage/App.js
+++ src/scenes/HomePage/App.js
@@ -13,24 +13,29 @@ class App extends Component {
     this.AnimatedText("code", ["understand", "design", "develop", "deploy"]);
 
   }
-  AnimatedText(target, texts, changeInterval, updateInterval, onTextChanged) {
-    let currentText = parseInt(Math.random() * texts.length);
-    let areaText = texts[0];
-    this.t1 = setInterval(() => {
-      let c = parseInt(Math.random() * Math.max(texts[currentText].length, areaText.length));
-      let s = texts[currentText][c];
-      if (typeof s == 'undefined') s = " ";
-      while (areaText.length < c) areaText += " ";
-      let newText = (areaText.slice(0, c) + s + areaText.slice(c + 1)).trim();
-      let diff = !(newText === areaText);
-      areaText = newText;
-      if (onTextChanged && diff) onTextChanged();
-      document.getElementById(target).innerHTML = areaText.length === 0 ? "&nbsp;" : areaText;
-    }, updateInterval ? updateInterval : 50);
-    this.t2 = setInterval(() => {
-      currentText = parseInt(Math.random() * texts.length);
-    }, changeInterval ? changeInterval : 2500);
-    this.stop = function () { clearInterval(this.t1); clearInterval(this.t2); }
+
+  componentWillUnmount() {
+    this.AnimatedText();
+  }
+  AnimatedText(target, texts, changeInterval, updateInterval) {
+    if (!target) {
+      clearInterval(this.t1, this.t2);
+    } else {
+      let currentText = parseInt(Math.random() * texts.length);
+      let areaText = texts[0];
+      this.t1 = setInterval(() => {
+        let c = parseInt(Math.random() * Math.max(texts[currentText].length, areaText.length));
+        let s = texts[currentText][c];
+        if (typeof s == 'undefined') s = " ";
+        while (areaText.length < c) areaText += " ";
+        let newText = (areaText.slice(0, c) + s + areaText.slice(c + 1)).trim();
+        areaText = newText;
+        document.getElementById(target).innerHTML = areaText.length === 0 ? "&nbsp;" : areaText;
+      }, updateInterval ? updateInterval : 50);
+      this.t2 = setInterval(() => {
+        currentText = parseInt(Math.random() * texts.length);
+      }, changeInterval ? changeInterval : 2500);
+    }
 
   }
 
@@ -38,7 +43,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="changingFont">
-          We <span id="code"></span>
+          We <span id="code">&nbsp;</span>
         </div>
       </div>
     );
--- src/services/Routes/index.js
+++ src/services/Routes/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom'
+import Particles from 'react-particles-js'
 import App from '../../scenes/HomePage/App';
 import ContactUs from '../../scenes/ContactUs';
 import Services from '../../scenes/Services';
@@ -15,7 +16,119 @@ class Router extends Component {
       }
     render() {
         return (
-            <div>
+            <div style={{position: "relative", height: "100%"}}>
+            <Particles
+              style={{"zIndex": "-1", "position": "absolute"}}
+              params={{
+                "particles": {
+                        "number": {
+                        "value": 80,
+                        "density": {
+                            "enable": true,
+                            "value_area": 800
+                        }
+                        },
+                        "color": {
+                        "value": "#E198C1"
+                        },
+                        "shape": {
+                        "type": "circle",
+                        "stroke": {
+                            "width": 0,
+                            "color": "#E198C1"
+                        },
+                        "polygon": {
+                            "nb_sides": 5
+                        },
+                        "image": {
+                            "width": 100,
+                            "height": 100
+                        }
+                        },
+                        "opacity": {
+                        "value": 0.5,
+                        "random": false,
+                        "anim": {
+                            "enable": false,
+                            "speed": 1,
+                            "opacity_min": 0.1,
+                            "sync": false
+                        }
+                        },
+                        "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 40,
+                            "size_min": 0.1,
+                            "sync": false
+                        }
+                        },
+                        "line_linked": {
+                        "enable": true,
+                        "distance": 150,
+                        "color": "#E198C1",
+                        "opacity": 0.4,
+                        "width": 1
+                        },
+                        "move": {
+                        "enable": true,
+                        "speed": 6,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 1200
+                        }
+                        }
+                    },
+                    "interactivity": {
+                        "detect_on": "window",
+                        "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        },
+                        "onclick": {
+                            "enable": true,
+                            "mode": "push"
+                        },
+                        "resize": true
+                        },
+                        "modes": {
+                        "grab": {
+                            "distance": 400,
+                            "line_linked": {
+                            "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 400,
+                            "size": 40,
+                            "duration": 2,
+                            "opacity": 8,
+                            "speed": 3
+                        },
+                        "repulse": {
+                            "distance": 200,
+                            "duration": 0.4
+                        },
+                        "push": {
+                            particles_nb: 4
+                        },
+                        remove: {
+                            particles_nb: 2
+                        }
+                        }
+                    },
+                    retina_detect: true
+                }}
+            />
                 <Switch>
                     <Route exact path='/' component={App} />
                     <Route path='/services' component={Services} />
