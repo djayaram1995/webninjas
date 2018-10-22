b4aafaf791155505ddb46c4b793d9271fafa21d7
unknown <jayaramdeenu@gmail.com>
2018-10-22 06:23:00 +0530

    full screen particles and lesser particles

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom'
+import Particles from 'react-particles-js'
 import './index.css';
 import logo from '../assets/logo.svg';
 import Router from '../services/Routes'
@@ -38,7 +39,119 @@ class RouterContainer extends Component {
         const { contactUsStyle, servicesStyle } = this.state;
         return (
             <BrowserRouter>
-                <div>
+                <div style={{position: "relative", height: "100%"}}>
+                <Particles
+              style={{"zIndex": "-1", "position": "absolute"}}
+              params={{
+                "particles": {
+                        "number": {
+                        "value": 80,
+                        "density": {
+                            "enable": true,
+                            "value_area": 2000
+                        }
+                        },
+                        "color": {
+                        "value": "#E198C1"
+                        },
+                        "shape": {
+                        "type": "circle",
+                        "stroke": {
+                            "width": 0,
+                            "color": "#E198C1"
+                        },
+                        "polygon": {
+                            "nb_sides": 5
+                        },
+                        "image": {
+                            "width": 100,
+                            "height": 100
+                        }
+                        },
+                        "opacity": {
+                        "value": 0.5,
+                        "random": false,
+                        "anim": {
+                            "enable": false,
+                            "speed": 1,
+                            "opacity_min": 0.1,
+                            "sync": false
+                        }
+                        },
+                        "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 40,
+                            "size_min": 0.1,
+                            "sync": false
+                        }
+                        },
+                        "line_linked": {
+                        "enable": true,
+                        "distance": 150,
+                        "color": "#E198C1",
+                        "opacity": 0.4,
+                        "width": 1
+                        },
+                        "move": {
+                        "enable": true,
+                        "speed": 6,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 1200
+                        }
+                        }
+                    },
+                    "interactivity": {
+                        "detect_on": "window",
+                        "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        },
+                        "onclick": {
+                            "enable": true,
+                            "mode": "push"
+                        },
+                        "resize": true
+                        },
+                        "modes": {
+                        "grab": {
+                            "distance": 400,
+                            "line_linked": {
+                            "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 400,
+                            "size": 40,
+                            "duration": 2,
+                            "opacity": 8,
+                            "speed": 3
+                        },
+                        "repulse": {
+                            "distance": 100,
+                            "duration": 0.4
+                        },
+                        "push": {
+                            particles_nb: 4
+                        },
+                        remove: {
+                            particles_nb: 2
+                        }
+                        }
+                    },
+                    retina_detect: true
+                }}
+            />
                     <div className="titleWebay">
                         <Link to="/" className="d-inline-block">
                             <img src={logo} alt="logo" style={{height: '50px', width: '50px'}} />
